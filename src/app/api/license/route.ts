c463162882aa3870e2075ba8e125f16f7977260a
TrejGun <trejgun@gmail.com>
2025-03-09 17:10:15 +0400

    feat: add supabase

---
+++ src/app/api/license/route.ts
@@ -0,0 +1,50 @@
+import type { NextRequest } from "next/server";
+import { encodeAbiParameters, Hash, keccak256 } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+
+import { supabase } from "../../../db/supabase";
+
+export async function POST(req: NextRequest) {
+  const { licenseKey, nonce } = await req.json();
+
+  const { data: licenseEntity } = await supabase //
+    .from("license")
+    .select()
+    .eq("uuid", licenseKey)
+    .single();
+
+  if (!licenseEntity) {
+    return Response.json({ message: "licenseNotFound" }, { status: 404 });
+  }
+
+  const { data: nonceEntity } = await supabase //
+    .from("nonce")
+    .select()
+    .eq("nonce", nonce)
+    .single();
+
+  if (nonceEntity) {
+    return Response.json({ message: "nonceExpired" }, { status: 400 });
+  }
+
+  await supabase //
+    .from("nonce")
+    .insert({
+      nonce,
+      license_uuid: licenseEntity.uuid,
+    });
+
+  const account = privateKeyToAccount(process.env.PRIVATE_KEY as Hash);
+
+  const payload = encodeAbiParameters(
+    [{ type: "bytes32" }, { type: "string" }, { type: "string" }],
+    [nonce, licenseKey, licenseEntity.status],
+  );
+
+  const signature = await account.signMessage({ message: { raw: keccak256(payload) } });
+
+  return Response.json({
+    signature,
+    nonce,
+  });
+}
--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,5 +1,17 @@
-export async function POST() {
+import type { NextRequest } from "next/server";
+
+import { supabase } from "../../../db/supabase";
+
+export async function POST(req: NextRequest) {
+  const { email } = await req.json();
+
+  const { error } = await supabase //
+    .from("subscription")
+    .insert({
+      email,
+    });
+
   return Response.json({
-    success: true,
+    success: !!error,
   });
 }
